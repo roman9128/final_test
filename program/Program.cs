c14fbe793bad3066932c84b29669fbc842d4e9b4
roman <roma-t@bk.ru>
2024-02-18 00:17:10 +0300

    added code for making array

--- program/Program.cs
+++ program/Program.cs
@@ -9,3 +9,54 @@
 [“Russia”, “Denmark”, “Kazan”] → []
 */
 
+Console.Clear();
+int MakeSize()
+{
+    Console.Write("Введите натуральное число: ");
+    string input = Console.ReadLine()!;
+    while (true)
+    {
+        int size;
+        if (int.TryParse(input, out size))
+        {
+            if (size > 0)
+            {
+                return size;
+            }
+            else
+            {
+                Console.WriteLine("Вы ввели число, которое не является натуральным. Попробуйте ещё раз: ");
+                input = Console.ReadLine()!;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели что-то, что не является натуральным числом. Попробуйте ещё раз. \nВведите натуральное число: ");
+            input = Console.ReadLine()!;
+        }
+    }
+}
+void PutTo(string[] array)
+{
+    int i = 0;
+    while (i < array.Length)
+    {
+        Console.Write($"Введите {i + 1}-ю строку: ");
+        array[i] = Console.ReadLine()!;
+        i++;
+    }
+}
+void Show(string[] array, int i = 0)
+{
+    if (i != array.Length)
+    {
+        Console.Write(array[i] + " ");
+        Show(array, i + 1);
+    }
+}
+Console.WriteLine("Вначале определим параметры массива");
+string[] text = new string[MakeSize()];
+PutTo(text);
+Console.Clear();
+Console.WriteLine("Вы ввели следующее:");
+Show(text);
